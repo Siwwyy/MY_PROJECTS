d7fed32ad6b705ef209a123d244855d432a3ae2d
Damian Andrysiak <andrydamian@gmail.com>
2019-07-09 18:04:52 +0200

    basic login window

--- Chat_Java/src/siwy/chat/Login.java
+++ Chat_Java/src/siwy/chat/Login.java
@@ -1,44 +1,71 @@
 package siwy.chat;
 
+//IMPORTS BELOW
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import java.awt.SystemColor;
 
-public class Login extends JFrame {
-
+public class Login extends JFrame 
+{
+	private static final long serialVersionUID = 1L;
+	
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
+	private JTextField txtName;
+	
+	//MAIN
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try 
+				{
 					Login frame = new Login();
 					frame.setVisible(true);
-				} catch (Exception e) {
+				} 
+				catch (Exception e)
+				{
 					e.printStackTrace();
 				}
 			}
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public Login() {
+	public Login() 
+	{
+		setFont(new Font("Dialog", Font.PLAIN, 13));
 		setTitle("Siwy Chat");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 505, 385);
+		setBounds(100, 100, 300, 450);
+		setSize(300,450);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.inactiveCaption);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null); //means it is a absolute layout, position of anything, anywhere you want
+		
+		txtName = new JTextField();
+		txtName.setHorizontalAlignment(SwingConstants.CENTER);
+		txtName.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		txtName.setBounds(10, 73, 274, 36);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Username:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 26, 274, 36);
+		contentPane.add(lblNewLabel);
 	}
-
 }
