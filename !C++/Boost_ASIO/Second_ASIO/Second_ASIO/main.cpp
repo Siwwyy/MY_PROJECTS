d7f2164bc6a1ba02ae1081dd7a4a4323347bf9c3
Damian Andrysiak <andrydamian@gmail.com>
2019-07-16 16:07:36 +0200

    added Second_ASIO project

---
+++ !C++/Boost_ASIO/Second_ASIO/Second_ASIO/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+#define NEW_LINE { '\n' }
+#define BOLD { '\e' }
+
+int main(int argc, char* argv[])
+{
+	using std::cout;
+	using std::endl;
+	using std::cin;
+
+	system("pause");
+	return 0;
+}
