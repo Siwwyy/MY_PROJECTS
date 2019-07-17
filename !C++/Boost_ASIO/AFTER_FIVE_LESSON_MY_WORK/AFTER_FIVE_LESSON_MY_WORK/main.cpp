f83ea7f03d8dc7a667c672f7bd9a5f99f4a98f25
Damian Andrysiak <andrydamian@gmail.com>
2019-07-17 20:57:17 +0200

    up

--- !C++/Boost_ASIO/AFTER_FIVE_LESSON_MY_WORK/AFTER_FIVE_LESSON_MY_WORK/main.cpp
+++ !C++/Boost_ASIO/AFTER_FIVE_LESSON_MY_WORK/AFTER_FIVE_LESSON_MY_WORK/main.cpp
@@ -16,13 +16,17 @@ private:
 	boost::asio::io_context::strand strand;
 	boost::asio::deadline_timer timer1;
 	boost::asio::deadline_timer timer2;
-	boost::asio::steady_timer timer3;
+	boost::asio::deadline_timer timer3;
+	boost::asio::deadline_timer timer4;
 	__int64 counter;
+	bool expires;
 public:
 	My_Class(boost::asio::io_context& io);
 	
 	void Multiply_Counter(const int multiply_by);
 	void Divide_Counter(const int divide_by);
+
+	int Get_Counter();
 
 	void Print();
 
@@ -58,46 +62,68 @@ My_Class::My_Class(boost::asio::io_context& io):
 	strand(io),
 	timer1(io, boost::posix_time::seconds(1)),
 	timer2(io, boost::posix_time::seconds(1)),
-	timer3(io, boost::asio::chrono::seconds(1)),
+	timer3(io, boost::posix_time::seconds(1)),
+	timer4(io, boost::posix_time::seconds(1)),
+	expires(false),
 	counter(2)
 {
 	timer1.async_wait(boost::asio::bind_executor(strand, boost::bind(&My_Class::Multiply_Counter,this, counter)));
-	//timer2.async_wait(boost::asio::bind_executor(strand, boost::bind(&My_Class::Divide_Counter, boost::asio::placeholders::error)));
 	timer2.async_wait(boost::asio::bind_executor(strand, boost::bind(&My_Class::Divide_Counter,this ,counter)));
-	//t.async_wait(boost::bind(print, boost::asio::placeholders::error, &t, &count));
-	timer3.async_wait(boost::bind(&My_Class::Print, this));
+	timer4.async_wait(boost::asio::bind_executor(strand, boost::bind<int>(&My_Class::Get_Counter,this)));
+	timer3.async_wait(boost::asio::bind_executor(strand, boost::bind(&My_Class::Print, this)));
+	std::cout << "BEGIN " << NEW_LINE;
 }
 
 void My_Class::Multiply_Counter(const int multiply_by)
 {
-	std::cout << "MULTI " << counter << ' '  << multiply_by << NEW_LINE;
-	this->counter *= multiply_by;
-	if (counter > 999999)
+	if (counter < 999999)
 	{
-		this->counter /= multiply_by;
+		std::cout << "MULTI " << counter << ' ' << multiply_by << NEW_LINE;
+		this->counter *= multiply_by;
+		timer1.expires_at(timer1.expires_at() + boost::posix_time::seconds(1));
+		timer1.async_wait(boost::asio::bind_executor(strand, boost::bind(&My_Class::Multiply_Counter, this, counter)));
 	}
-	timer1.expires_at(timer1.expires_at() + boost::posix_time::seconds(1));
-	timer1.async_wait(boost::asio::bind_executor(strand, boost::bind(&My_Class::Multiply_Counter, this, counter)));
+	else
+	{
+		expires = true;
+	}
 }
 
 void My_Class::Divide_Counter(const int divide_by)
 {
-	std::cout << "DIVIDI " << counter << ' ' << divide_by << NEW_LINE;
-	this->counter /= divide_by; 
-	//if (counter < 2)
-	//{
-	//	counter = 2;
-	//}
-//	std::cin.get();
-	timer2.expires_at(timer2.expires_at() + boost::posix_time::seconds(1));
-	timer2.async_wait(boost::asio::bind_executor(strand, boost::bind(&My_Class::Divide_Counter, this, counter)));
+	if (expires == false)
+	{
+		std::cout << "DIVIDI " << counter << ' ' << divide_by << NEW_LINE;
+		this->counter /= divide_by;
+		timer2.expires_at(timer2.expires_at() + boost::posix_time::seconds(1));
+		timer2.async_wait(boost::asio::bind_executor(strand, boost::bind(&My_Class::Divide_Counter, this, counter)));
+	}
+}
+
+int My_Class::Get_Counter()
+{
+	std::cout << "GET COUNTER " << NEW_LINE;
+	timer4.expires_at(timer4.expires_at() + boost::posix_time::seconds(1));
+	return this->counter;
+	timer4.async_wait(boost::asio::bind_executor(strand, boost::bind<int>(&My_Class::Get_Counter, this)));
 }
 
 void My_Class::Print()
 {
-	//std::cout << this->counter << NEW_LINE;
-	timer3.expires_at(timer3.expires_at() + boost::asio::chrono::seconds(1));
-	timer3.async_wait(boost::bind(&My_Class::Print, this));
+	if (expires == false)
+	{
+		std::cout << "PRINT " << Get_Counter() << NEW_LINE;
+		timer3.expires_at(timer3.expires_at() + boost::posix_time::seconds(1));
+		timer3.async_wait(boost::bind(&My_Class::Print, this));
+		std::cout << NEW_LINE;
+	}
+	else
+	{
+		timer1.expires_from_now();
+		timer2.expires_from_now();
+		timer3.expires_from_now();
+		timer4.expires_from_now();
+	}
 }
 
 My_Class::~My_Class()
