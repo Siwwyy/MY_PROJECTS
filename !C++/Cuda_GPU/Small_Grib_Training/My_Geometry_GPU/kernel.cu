9953305aaeab08af2cb8d7e0ecabee57d12175b8
Damian Andrysiak <andrydamian@gmail.com>
2019-07-24 21:47:38 +0200

    small up

--- !C++/Cuda_GPU/Small_Grib_Training/My_Geometry_GPU/kernel.cu
+++ !C++/Cuda_GPU/Small_Grib_Training/My_Geometry_GPU/kernel.cu
@@ -15,7 +15,8 @@ using namespace std;
 ////////////////////////////////////////////////////
 //GPU
 
-#define DIM 1000
+#define DIM 100
+#define OBJECT_SIZE (DIM/2)
 
 class _Point
 {
@@ -23,13 +24,15 @@ private:
 	/*
 		ZMIENNE PRIVATE
 	*/
-
+	float x;
+	float y;
 	//////////////////////////////////////////////////////////////////////////////
 public:
 	/*
 		KONSTRUKTORY PUBLIC
 	*/
-	_Point();
+	_Point() = delete;
+	__device__ _Point(const float x, const float y);
 	//////////////////////////////////////////////////////////////////////////////
 	/*
 		FUNKCJE PUBLIC
@@ -39,33 +42,138 @@ public:
 	/*
 		SETTERY PUBLIC
 	*/
-
+	__device__ void Set_X(const float* new_X);
+	__device__ void Set_Y(const float* new_Y);
 	//////////////////////////////////////////////////////////////////////////////
 	/*
 		GETTERY PUBLIC
 	*/
-
+	__device__ constexpr float Get_X() const;
+	__device__ constexpr float Get_Y() const;
 	//////////////////////////////////////////////////////////////////////////////
 	/*
 		OPERATORY PUBLIC
 	*/
 	//JEDNOARGUMENTOWE
-
+	__device__ _Point& operator+(const _Point& Object);
+	__device__ _Point& operator-(const _Point& Object);
+	__device__ _Point& operator*(const _Point& Object);
 	//////////////////////////////////////////////////////////////////////////////
 	/*
 		DESTRUKTOR
 	*/
-	 ~_Point();		//wirtualny destruktor
+	__device__ ~_Point();		//wirtualny destruktor
 	//////////////////////////////////////////////////////////////////////////////
 };
 
+__global__ void Kernel(unsigned char* ptr);
+__device__ int Create_Square(int x, int y);
 
 int main(int argc, char* argv[])
 {
 	CPUBitmap bitmap(DIM, DIM);
 	unsigned char* dev_bitmap{};
 
+	HANDLE_ERROR(cudaMalloc((void**)& dev_bitmap, bitmap.image_size()));
+	dim3 grid(DIM, DIM);
+
+	Kernel<<<grid, 1 >>>(dev_bitmap);
+
+	HANDLE_ERROR(cudaMemcpy(bitmap.get_ptr(), dev_bitmap, bitmap.image_size(), cudaMemcpyDeviceToHost));
+
+	bitmap.display_and_exit();
+	HANDLE_ERROR(cudaFree(dev_bitmap));
 
 	system("pause");
 	return 0;
+}
+
+// CLASS POINT CASE
+
+__device__ _Point::_Point(const float x, const float y):
+	x(x),
+	y(y)
+{
+
+}
+
+__device__ void _Point::Set_X(const float* new_X)
+{
+	this->x = (*new_X);
+}
+
+__device__ void _Point::Set_Y(const float* new_Y)
+{
+	this->y = (*new_Y);
+}
+
+__device__ constexpr float _Point::Get_X() const
+{
+	return this->x;
+}
+
+__device__ constexpr float _Point::Get_Y() const
+{
+	return this->y;
+}
+
+__device__ _Point& _Point::operator+(const _Point& Object)
+{
+	this->x += Object.x;
+	this->y += Object.y;
+	return *this;
+}
+
+__device__ _Point& _Point::operator-(const _Point& Object)
+{
+	this->x -= Object.x;
+	this->y -= Object.y;
+	return *this;
+}
+
+__device__ _Point& _Point::operator*(const _Point& Object)
+{
+	this->x *= Object.x;
+	this->y *= Object.y;
+	return *this;
+}
+
+_Point::~_Point()
+{
+	this->x = 0;
+	this->y = 0;
+}
+
+////////////////////////////////////////////////////
+
+__global__ void Kernel(unsigned char* ptr)
+{
+	// Odwzorowanie z blockldx na współrzędne piksela
+	int x = blockIdx.x;
+	int y = blockIdx.y;
+	int offset = x + y * gridDim.x;
+	// Obliczenie wartości dla tego punktu
+	int my_value = Create_Square(x, y);
+	ptr[offset * 4 + 0] = 255 * my_value;
+	ptr[offset * 4 + 1] = 0;
+	ptr[offset * 4 + 2] = 0;
+	ptr[offset * 4 + 3] = 255;
+}
+
+__device__ int Create_Square(int x, int y)
+{
+	const float scale = 1.5;
+	float jx = scale * (float)(DIM / 2 - x) / (DIM / 2);
+	float jy = scale * (float)(DIM / 2 - y) / (DIM / 2);
+
+	_Point Object(jx, jy);
+
+	for (size_t i = 0; i < 200; i++)
+	{
+		if (Object.Get_X() > OBJECT_SIZE)
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
